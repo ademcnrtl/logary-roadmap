ac9934a42446a8b04c56d1a01e18013cdbba08e6
Adem Can Certel <ademcan.cert@gmail.com>
2026-03-23 03:00:43 +0300

    Logary PWA: İlk kurulum ve altyapı

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'logary-roadmap-cache-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  'https://fonts.googleapis.com/css2?family=Syne:wght@400;600;700;800&family=DM+Sans:wght@300;400;500&display=swap',
+  'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
